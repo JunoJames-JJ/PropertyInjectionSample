2ecfd7d7ed0aa0d376e49ead19d1594e76edc06e
junojobin <junojamesv@gmail.com>
2024-05-06 18:47:48 -0500

    Code Changes..

--- PropertyInjectionSample.playground/Contents.swift
+++ PropertyInjectionSample.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct loggedUserIn {
+    let username: String
+}
+
+class ApiClient {
+    static let instance = ApiClient ()
+    private init () { }
+    
+    func login (completion: (loggedUserIn) -> Void) {
+        print ("Login button called...")
+        let user = loggedUserIn(username: "ExampleUser")
+        completion(user)
+    }
+}
+
+class LoginViewController: UIViewController {
+    //Property Injection...
+    var api = ApiClient.instance
+    func didTapLoginButton () {
+       api.login () { user in
+           print ("Logged in as \(user.username)")
+        }
+    }
+}
+
+let viewController = LoginViewController()
+viewController.didTapLoginButton()
+
+
+
